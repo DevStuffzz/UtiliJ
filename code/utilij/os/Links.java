40357eb6e90ef857ec57c0edd2545c337d73b4aa
DevStuffz <67448356+DevStuffzz@users.noreply.github.com>
2021-03-05 10:11:37 -0500

    Update Links.java

--- code/utilij/os/Links.java
+++ code/utilij/os/Links.java
@@ -1,7 +1,7 @@
 package utilij.os;
 
 public class Links {
-    public static final String DEV_WEBSITE = "https://devstuffzz.github.io";
+    public static final String DEV_WEBSITE = "https://devstuffzz.github.io/CoreyBeaver";
     public static final String YOUTUBE = "www.youtube.com";
     public static final String GITHUB = "www.github.com";
     public static final String ITCH = "www.itch.io";
